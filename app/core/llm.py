bbc563d498c477137015e2ada0578b3ccf74016a
Sudhakar <om.sudhakarrecruit@gmail.com>
2026-06-17 17:02:06 +0530

    LLM name corrected

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,20 +1,21 @@
 # loading  model using .env 
 import os
+from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 load_dotenv()
 
-groq_model=str(os.getenv("GROQ_MODEL"))
-groq_model2=str(os.getenv("GROQ_MODEL2"))
+GROQ_MODEL=str(os.getenv("GROQ_MODEL"))
+GROQ_MODEL2=str(os.getenv("GROQ_MODEL2"))
 # print(groq_model)
 # print(groq_model2)
-from langchain_groq import ChatGroq
+
 llm=ChatGroq(
-    model=groq_model,
+    model=GROQ_MODEL,
     temperature=0.1
 )
 
 llm2=ChatGroq(
-    model=groq_model2,
+    model=GROQ_MODEL2,
     temperature=0.1
 )
 
